625d8b42062ab8e290a639073e3071fd6feefeee
gulativibhav <gulativibhav@gmail.com>
2017-10-25 14:21:58 +0530

    added

--- pr.sh
+++ pr.sh
@@ -1,1 +1,1 @@
-git checkout master && git checkout -b $1 && touch .$2 && git add . && git commit -m  '100 keep' && git push origin $1 && git checkout master
+git checkout master && git checkout -b $1 && echo 123 > README.md && git add . && git commit -m 'eat sleep added repeat' && git push origin $1 && git checkout master
